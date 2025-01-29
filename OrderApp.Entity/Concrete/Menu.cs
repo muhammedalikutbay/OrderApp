0e42c47469e5041543f5e8a7cae49742d0a10df3
Muhammed Ali Kutbay <muhammedalikutbay@gmail.com>
2025-01-30 02:24:23 +0300

    created architecture and edited entitylayer

---
+++ OrderApp.Entity/Concrete/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApp.Entity.Concrete
+{
+    internal class Menu
+    {
+    }
+}
